864c45b46d560b1ee962ab2e66fc02dbc5bd85e8
Porter <48492361+uPorter@users.noreply.github.com>
2023-06-03 15:48:40 +0300

    Pending Status fix

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -3,17 +3,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Dropzone from 'react-dropzone';
 import Button from '@mui/joy/Button';
-import CircularProgress from '@mui/joy/CircularProgress';
 import { Toaster, toast } from "sonner";
-import GlobalStyles from "@mui/joy/GlobalStyles";
-import Alert from "@mui/joy/Alert";
-import Box from "@mui/joy/Box";
-import LinearProgress from "@mui/joy/LinearProgress";
-import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
-import Close from "@mui/icons-material/Close";
-import WarningIcon from "@mui/icons-material/Warning";
-import ToastContent from './ToastComponent';
 
 function FileUpload() {
   const [file, setFile] = useState(null);
@@ -43,39 +34,6 @@ function FileUpload() {
 
   return (
     <div className='fileUploadContainer'>
-      <GlobalStyles
-        styles={(theme) => `
-        [data-sonner-toaster][data-theme] {
-          font-family: ${theme.vars.fontFamily.body};
-          font-size: ${theme.fontSize.md};
-          --border-radius: ${theme.vars.radius.sm};
-          --normal-bg: ${theme.vars.palette.background.surface};
-          --normal-border: ${theme.vars.palette.divider};
-          --normal-text: ${theme.vars.palette.text.primary};
-          --success-bg: ${theme.vars.palette.success.softBg};
-          --success-border: rgb(${theme.vars.palette.success.mainChannel} / 0.2);
-          --success-text: ${theme.vars.palette.success.softColor};
-          --error-bg: ${theme.vars.palette.danger.softBg};
-          --error-border: rgb(${theme.vars.palette.danger.mainChannel} / 0.2);
-          --error-text: ${theme.vars.palette.danger.softColor};
-          --gray1: ${theme.vars.palette.neutral[50]};
-          --gray2: ${theme.vars.palette.neutral[100]};
-          --gray3: ${theme.vars.palette.neutral[200]};
-          --gray4: ${theme.vars.palette.neutral[300]};
-          --gray5: ${theme.vars.palette.neutral[400]};
-          --gray6: ${theme.vars.palette.neutral[500]};
-          --gray7: ${theme.vars.palette.neutral[600]};
-          --gray8: ${theme.vars.palette.neutral[700]};
-          --gray9: ${theme.vars.palette.neutral[800]};
-          --gray10: ${theme.vars.palette.neutral[900]};
-        }
-        &.sonner-toast-warn {
-          --normal-bg: ${theme.vars.palette.warning.softBg};
-          --normal-border: rgb(${theme.vars.palette.warning.mainChannel} / 0.2);
-          --normal-text: ${theme.vars.palette.warning.softColor};
-        }
-      `}
-      />
       <div className='fileUploaderHolder'>
         <Dropzone className='fileDropZone' onDrop={(acceptedFiles) => setFile(acceptedFiles[0])}>
           {({ getRootProps, getInputProps }) => (
