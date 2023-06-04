a701ab0e16a3b73ae10bf37d1fa11163bb3006ae
Porter <48492361+uPorter@users.noreply.github.com>
2023-06-04 13:22:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import styles from './App.css';
 import UnityLoader from './components/UnityLoader';
+import { ChangeEvent, useState, useEffect, useCallback } from 'react';
 
 function App() {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragEnter = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+
+    // Sürüklenen dosyaların işlemlerini burada yapabilirsiniz
+    const files = event.dataTransfer.files;
+    console.log('Sürüklenen dosyalar:', files);
+  };
+
   return (
-    <div className={styles.container}>
+    <div
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop} 
+      style={{backgroundColor: isDragging ? 'lightblue' : 'white' }}
+      className={styles.container}>
       <UnityLoader></UnityLoader>
+
     </div>
   );
 }
