43a5f6ab9fd81fb23a798dd7cf28a3ed6910f0cf
Porter <48492361+uPorter@users.noreply.github.com>
2023-06-05 17:20:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from './App.css';
 import UnityLoader from './components/UnityLoader';
-import { ChangeEvent, useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [isDragging, setIsDragging] = useState(false);
@@ -18,13 +19,38 @@ function App() {
     setIsDragging(false);
   };
 
-  const handleDrop = (event) => {
+  const handleDrop = async (event) => {
     event.preventDefault();
     setIsDragging(false);
 
     // Sürüklenen dosyaların işlemlerini burada yapabilirsiniz
     const files = event.dataTransfer.files;
     console.log('Sürüklenen dosyalar:', files);
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+      
+      try {
+        const response = await axios.post('https://3ec8-152-32-192-31.ngrok-free.app/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          onUploadProgress: (progressEvent) => {
+            const percentage = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+            console.log('Yükleme Yüzdesi:', percentage);
+          },
+        });
+        console.log('Yükleme Tamamlandı:', response.data);
+        window.sendMessageToUnity("urlManager", "setURL", response.data);
+        window.sendMessageToUnity("urlManager", "SpawnObject");
+        // İstediğiniz işlemleri burada gerçekleştirebilirsiniz
+        // Örneğin, UnityLoader'a mesaj göndermek
+      } catch (error) {
+        console.log('Yükleme Hatası:', error);
+      }
+    }
   };
 
   return (
@@ -32,11 +58,11 @@ function App() {
       onDragEnter={handleDragEnter}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}
-      onDrop={handleDrop} 
-      style={{backgroundColor: isDragging ? 'lightblue' : 'white' }}
-      className={styles.container}>
+      onDrop={handleDrop}
+      style={{ backgroundColor: isDragging ? 'lightblue' : 'white' }}
+      className={styles.container}
+    >
       <UnityLoader></UnityLoader>
-
     </div>
   );
 }
