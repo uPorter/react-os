d838da43d41a28c8a36c8a85986a334e56baf5a5
Porter <48492361+uPorter@users.noreply.github.com>
2023-06-10 13:41:45 +0300

    Update ChatComponent.js

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -124,7 +124,7 @@ const ChatComponent = (props) => {
   }
   return (
     <>
-      <div className='chatProfileSection' style={{ width: '400px', height: 'fit-content', position: 'absolute', right: '8px', bottom: '-42px', transform: 'scale(0.9)', zIndex: '50', userSelect: 'none' }} >
+      <div className='chatProfileSection' style={{ width: '400px', height: 'fit-content', position: 'absolute', right: '8px', bottom: '-41px', transform: 'scale(0.9)', zIndex: '50', userSelect: 'none' }} >
         <Chat client={chatClient} theme='livestream light' user={chatClient.user} userToken={userToken}>
           <Channel channel={chatClient.channel('livestream', 'BellyRub')} Message={MessageLivestream}>
             <Window hideOnThread>
