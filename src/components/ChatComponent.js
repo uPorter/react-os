aa7dac0b933c4e6770a7cd67f49ffffd9e00edfe
Porter <48492361+uPorter@users.noreply.github.com>
2023-06-23 23:55:37 +0300

    BrotherHood

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -124,7 +124,7 @@ const ChatComponent = (props) => {
 
   return (
     <>
-      <div className='chatProfileSection' style={{ width: '400px', height: 'fit-content', position: 'absolute', right: '8px', bottom: '-41px', transform: 'scale(0.9)', zIndex: '50', userSelect: 'none' }} >
+      <div className='chatProfileSection' style={{pointerEvents: 'none', width: '400px', height: 'fit-content', position: 'absolute', right: '8px', bottom: '-41px', transform: 'scale(0.9)', zIndex: '50', userSelect: 'none' }} >
         <Chat client={chatClient} theme='livestream light' user={chatClient.user} userToken={userToken}>
           <Channel channel={chatClient.channel('livestream', spaceName || 'demoroom')} Message={MessageLivestream}>
             <Window hideOnThread>
