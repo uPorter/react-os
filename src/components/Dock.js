d1c4d99ad1231c36772b9fa69ea2eca830ec7139
Porter <48492361+uPorter@users.noreply.github.com>
2023-08-15 15:19:26 +0300

    Update Dock.js

--- src/components/Dock.js
+++ src/components/Dock.js
@@ -234,7 +234,7 @@ function Dock({ handleAddContent }) {
 
         <Tooltip  className='dockTooltip' sx={{ borderRadius: '20px', backgroundColor: '#ffffff' }} interactive color="neutral" placement="top" variant="soft" title={<Button size="sm" variant="plain" sx={{
           fontStyle: 'bold',
-          visiblity: !isReactionsOn ? 'visible' : 'hidden',
+          visiblity: !isReactionsOn ? 'visible!important' : 'hidden!important',
           fontWeight: 'Bold',
           color: 'white',
           padding: '10px',
